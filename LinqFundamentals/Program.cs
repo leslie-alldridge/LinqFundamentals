225f08eead718d5c693be1446b3663ed8718d449
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-25 21:20:34 +1300

    method/query syntax

--- LinqFundamentals/Program.cs
+++ LinqFundamentals/Program.cs
@@ -17,7 +17,8 @@ namespace LinqFundamentals
             {
                 new Employee {Id = 1, Name = "Leslie"},
                 new Employee {Id = 2, Name = "Lisa"},
-                new Employee {Id = 3, Name = "Steven"}
+                new Employee {Id = 3, Name = "Steven"},
+                new Employee {Id = 4, Name = "Lucy"}
             };
 
             var sales = new List<Employee>()
@@ -25,7 +26,23 @@ namespace LinqFundamentals
                 new Employee{ Id = 3, Name = "Alex"}
             };
 
-            foreach (var employee in developers.Where(e => e.Name.StartsWith("L")).OrderByDescending(e => e.Name))
+            var querySyntax = from developer in developers
+                where developer.Name.Length == 4
+                orderby developer.Name
+                select developer;
+
+            var methodSyntax = developers.Where(e => e.Name.StartsWith("L"))
+                .OrderByDescending(e => e.Name)
+                .Select(e => e);
+
+            foreach (var employee in methodSyntax)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.WriteLine("*****");
+
+            foreach (var employee in querySyntax)
             {
                 Console.WriteLine(employee.Name);
             }
