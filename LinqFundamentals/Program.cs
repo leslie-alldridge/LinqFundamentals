0cc689ca2588e53b928ff1f5f349434448391b0f
leslie.alldridge <leslie.alldridge@xero.com>
2020-11-25 20:26:55 +1300

    Add project files.

---
+++ LinqFundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var developers = new[]
+            {
+                new Employee {Id = 1, Name = "Leslie"},
+                new Employee {Id = 2, Name = "Lisa"}
+            };
+
+            var sales = new List<Employee>()
+            {
+                new Employee{ Id = 3, Name = "Alex"}
+            };
+        }
+    }
+}
