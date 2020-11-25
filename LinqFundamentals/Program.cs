986ff2e49a4bcfc26a995367d53c0df0c2bb8ae5
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-25 20:50:34 +1300

    .where lambda function

--- LinqFundamentals/Program.cs
+++ LinqFundamentals/Program.cs
@@ -13,13 +13,25 @@ namespace LinqFundamentals
             var developers = new[]
             {
                 new Employee {Id = 1, Name = "Leslie"},
-                new Employee {Id = 2, Name = "Lisa"}
+                new Employee {Id = 2, Name = "Lisa"},
+                new Employee {Id = 3, Name = "Steven"}
             };
 
             var sales = new List<Employee>()
             {
                 new Employee{ Id = 3, Name = "Alex"}
             };
+
+            foreach (var employee in developers.Where(e => e.Name.StartsWith("S")))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+        }
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
         }
     }
 }
